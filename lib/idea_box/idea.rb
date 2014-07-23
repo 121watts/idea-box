08f833b7ba3906d422be5d0de1cf42a4c8f2316d
Andrew Watkins <andrew.watkinz@gmail.com>
2014-07-23 09:06:04 -0600

    end of tuttorial everything works

--- lib/idea_box/idea.rb
+++ lib/idea_box/idea.rb
@@ -2,11 +2,34 @@ require 'yaml/store'
 
 class Idea
 
-  attr_reader :title, :description
+  include Comparable
+  attr_reader :title, :description, :id, :rank
 
   def initialize(attributes)
+    @id          = attributes["id"]
     @title       = attributes["title"]
     @description = attributes["description"]
+    @rank        = attributes["rank"] || 0
+  end
+
+  def <=>(other)
+    other.rank <=> rank
+  end
+
+  def to_h
+    {
+      "title" => title,
+      "description" => description,
+      "rank" => rank
+    }
+  end
+
+  def like!
+    @rank += 1
+  end
+
+  def save
+    IdeaStore.create(to_h)
   end
 
 end
